4f5aa05c21440e0c15a660017b3e32e8dd70065a
Gastón Montes <gmontes@tbxnet.com>
2017-10-05 02:22:44 -0300

    Gaston - 75.42 - Add latest files.

---
+++ 75.42 - Taller de programación/Veiga/Material Clases/Código Threads/mutex.h
@@ -0,0 +1,10 @@
+#include <pthread.h>
+
+typedef struct {
+pthread_mutex_t mutex;
+} mutex_t;
+
+void mutex_create(mutex_t* self);
+void mutex_destroy(mutex_t* self);
+void mutex_lock(mutex_t* self);
+void mutex_unlock(mutex_t* self);
